bf1f0c53a0220858dbaad64cbfa5f91ee66eefc8
shagufa-anjum1 <shagufa.anjum2021@vitbhopal.ac.in>
2025-06-06 09:00:01 +0530

    Routing Request

--- Lecture 3 -First Node Server/app.js
+++ Lecture 3 -First Node Server/app.js
@@ -5,13 +5,43 @@ const http = require('http');
 // }
 
 const server = http.createServer((req, res) => {
-   console.log(req);
+   console.log(req.url, req.method, req.headers);
+
+   if(req.url === '/'){
+   res.setHeader('Content-Type', 'text/html');
+   res.write('<html>');
+   res.write('<head><title>Complete Coding</title></head>');
+   res.write('<body><h1>Welcome to our Website !</h1></body>');
+   res.write('</html>');
+   res.end();
+   return res.end();
+   } else if(req.url === '/products'){
+   res.setHeader('Content-Type', 'text/html');
+   res.write('<html>');
+   res.write('<head><title>Complete Coding</title></head>');
+   res.write('<body><h1>Checkout your Products! </h1></body>');
+   res.write('</html>');
+   res.end();
+   return res.end();
+   }
+   else{
+   res.setHeader('Content-Type', 'text/html');
+   res.write('<html>');
+   res.write('<head><title>Complete Coding</title></head>');
+   res.write('<body><h1>Subscribe Shagufa Anjum Channel!</h1></body>');
+   res.write('</html>');
+   res.end();
+   return res.end();
+   }
+
+   
+   
  });
 
  const PORT = 3001;
 
  server.listen(PORT, ()=> {
 
-  console.log(`Server running on address http:localhost:${PORT}`);
+  console.log(`Server running on address http://localhost:${PORT}`);
 
  });
